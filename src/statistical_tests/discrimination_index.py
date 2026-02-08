Fixed the discrimination index script (there was an error with how the variance (S2) variable was being calculated and it broke the math.sqrt function). Also added testing of the discrimination index function using test data, DI function test currently passes

src/statistical_tests/discrimination_index.py
@@ -14,10 +14,9 @@ def DiscriminationIndex(sm):
     s  = np.unique(sm, return_counts=True)
     summation_list =[]
     for i in s[1]:
-        y = i * (i-1)
+        y = (i * (i-1))
         summation_list.append(y)
     D = 1 - (sum(summation_list)) / (N * (N-1))
-
     #95% CI calculation (Grundmann, Hori & Tanner, 2001)
 
     summation_pi_j3 = []
@@ -29,10 +28,12 @@ def DiscriminationIndex(sm):
         summation_pi_j3.append(pi_j3)
         summation_pi_j2.append(pi_j2)
 
-    S2 = (4 * (sum(summation_pi_j3)) - ((sum(summation_pi_j2) ** 2)**2))))
-    CI_low = D - (2 * math.sqrt(S2))    
-    CI_high = D + (2 * math.sqrt(S2)) 
+    pi_j3 = sum(summation_pi_j3)
+    pi_j2 = (sum(summation_pi_j2) ** 2)
+    S2 = (4/N) * (pi_j3 - pi_j2)
+    CI_low = (D - (2 * math.sqrt(S2)))
+    CI_high = (D + (2 * math.sqrt(S2)))
 
-    return(D, CI_low, CI_high)
+    return(round(D, 3), round(CI_low, 3), round(CI_high, 3))
 
 